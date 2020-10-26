aa5561a5f251b5a48c37e4ff98e256ae8ca1f51a
SumayyaAzzony <70629380+SumayyaAzzony@users.noreply.github.com>
2020-10-26 21:38:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,454 @@
+
+
+//Random Color function
+function randomColor(){
+  var ranColNum = Math.random(); //ranColNum is a random number for color
+ ranColNum =ranColNum*9;
+ ranColNum=Math.floor(ranColNum)+1;
+  if(ranColNum==1){
+document.querySelector(".first").innerHTML =ranColNum;
+document.getElementById("he1").innerHTML="1";
+}
+else if(ranColNum==2)
+{
+  document.querySelector(".first").style.backgroundColor ="orange";
+document.getElementById("he1").innerHTML="2";
+}
+else if(ranColNum==3)
+{
+  document.querySelector(".first").style.backgroundColor ="yellow";
+document.getElementById("he1").innerHTML="3";
+
+}
+else if(ranColNum==4)
+{
+  document.querySelector(".first").style.backgroundColor ="green";
+document.getElementById("he1").innerHTML="4";
+}
+else if(ranColNum==5)
+{
+  document.querySelector(".first").style.backgroundColor ="lightBlue";
+document.getElementById("he1").innerHTML="5";
+}
+else if(ranColNum==6)
+{
+  document.querySelector(".first").style.backgroundColor ="blue";
+document.getElementById("he1").innerHTML="6";
+}
+else if(ranColNum==7)
+{
+  document.querySelector(".first").style.backgroundColor ="white";
+document.getElementById("he1").innerHTML="7";
+}
+else if(ranColNum==8)
+{
+  document.querySelector(".first").style.backgroundColor ="gray";
+  document.getElementById("he1").innerHTML="8";
+
+}
+else
+{
+  document.querySelector(".first").style.backgroundColor ="pink";
+document.getElementById("he1").innerHTML="9";
+}
+// the second
+ ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".second").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".second").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".second").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".second").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".second").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".second").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".second").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".second").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".second").style.backgroundColor ="pink";
+}
+//for the third
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".third").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".third").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".third").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".third").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".third").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".third").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".third").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".third").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".third").style.backgroundColor ="pink";
+}
+//for the fourth
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".fourth").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".fourth").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".fourth").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".fourth").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".fourth").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".fourth").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".fourth").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".fourth").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".fourth").style.backgroundColor ="pink";
+}
+//for the fifth
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".fifth").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".fifth").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".fifth").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".fifth").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".fifth").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".fifth").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".fifth").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".fifth").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".fifth").style.backgroundColor ="pink";
+}
+//for the sixth
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".sixth").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".sixth").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".sixth").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".sixth").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".sixth").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".sixth").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".sixth").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".sixth").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".sixth").style.backgroundColor ="pink";
+}
+//for the seventh
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".seventh").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".seventh").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".seventh").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".seventh").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".seventh").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".seventh").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".seventh").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".seventh").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".seventh").style.backgroundColor ="pink";
+}
+//for the eightth
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".eightth").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".eightth").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".eightthh").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".eightth").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".eightth").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".eightth").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".eightth").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".eightth").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".eightth").style.backgroundColor ="pink";
+}
+//for the ninth
+ranColNum = Math.random(); //ranColNum is a random number for color
+ranColNum =ranColNum*9;
+ranColNum=Math.floor(ranColNum)+1;
+ if(ranColNum==1)
+document.querySelector(".ninth").style.backgroundColor ="red";
+
+else if(ranColNum==2)
+{
+ document.querySelector(".ninth").style.backgroundColor ="orange";
+}
+else if(ranColNum==3)
+{
+ document.querySelector(".ninth").style.backgroundColor ="yellow";
+}
+else if(ranColNum==4)
+{
+ document.querySelector(".ninth").style.backgroundColor ="green";
+}
+else if(ranColNum==5)
+{
+ document.querySelector(".ninth").style.backgroundColor ="lightBlue";
+}
+else if(ranColNum==6)
+{
+ document.querySelector(".ninth").style.backgroundColor ="blue";
+}
+else if(ranColNum==7)
+{
+ document.querySelector(".ninth").style.backgroundColor ="white";
+}
+else if(ranColNum==8)
+{
+ document.querySelector(".ninth").style.backgroundColor ="gray";
+}
+else
+{
+ document.querySelector(".ninth").style.backgroundColor ="pink";
+}
+}
+//----------------------------------------------------------------------
+//DisappearingNumbers function
+function disappearingNumbers(){
+document.querySelector(".first").style.backgroundColor ="lightBlue";
+document.querySelector(".second").style.backgroundColor ="lightBlue";
+document.querySelector(".third").style.backgroundColor ="lightBlue";
+document.querySelector(".fourth").style.backgroundColor ="lightBlue";
+//document.querySelector(".fifth").style.backgroundColor ="lightBlue";
+// Hint: no need to change the background color of the fifth, it is already light blue.
+document.querySelector(".sixth").style.backgroundColor ="lightBlue";
+document.querySelector(".seventh").style.backgroundColor ="lightBlue";
+document.querySelector(".eightth").style.backgroundColor ="lightBlue";
+document.querySelector(".ninth").style.backgroundColor ="lightBlue";
+var array; //this array to save numbered disappeared
+
+//if(mo1=='true'&& mo2=='true' && mo3=='true'&& mo4=='true'&& mo5=='true'&& mo6=='true'&& mo7=='true'&& mo8=='true'&& mo9=='true')
+//alert("all the numbers are disappeared");
+//for the first
+//var counter=0;
+document.querySelector(".first").addEventListener("mouseover",function(event){
+  document.querySelector(".first").style.visibility="hidden";
+  array.push("1");
+  if(array.length==9){
+  alert("the order in which the numbers disappeared is:"+array);}});
+
+//  counter++;
+  //for the second
+  document.querySelector(".second").addEventListener("mouseover",function(event){
+    document.querySelector(".second").style.visibility="hidden";
+    array.push("2");
+    if(array.length==9){
+    alert("the order in which the numbers disappeared is:"+array);}});
+
+    //for the third
+    document.querySelector(".third").addEventListener("mouseover",function(event){
+      document.querySelector(".third").style.visibility="hidden";
+      array.push("3");
+      if(array.length==9){
+      alert("the order in which the numbers disappeared is:"+array);}});
+  //    array.push('3');
+      //the fourth
+      document.querySelector(".fourth").addEventListener("mouseover",function(event){
+        document.querySelector(".fourth").style.visibility="hidden";
+        array.push("4");
+        if(array.length==9){
+        alert("the order in which the numbers disappeared is:"+array);}});
+    //    array.push('4');
+      //the fifth
+      document.querySelector(".fifth").addEventListener("mouseover",function(event){
+        document.querySelector(".fifth").style.visibility="hidden";
+        array.push("5");
+        if(array.length==9){
+        alert("the order in which the numbers disappeared is:"+array);}});
+      //  array.push('5');
+        //the sixth
+        document.querySelector(".sixth").addEventListener("mouseover",function(event){
+          document.querySelector(".sixth").style.visibility="hidden";
+          array.push("6");
+          if(array.length==9){
+          alert("the order in which the numbers disappeared is:"+array);}});
+      //    array.push('6');
+          //for the seventh
+          document.querySelector(".seventh").addEventListener("mouseover",function(event){
+            document.querySelector(".seventh").style.visibility="hidden";
+            array.push("7");
+            if(array.length==9){
+            alert("the order in which the numbers disappeared is:"+array);}});
+      //      array.push('7');
+          //for the seventh
+          document.querySelector(".eightth").addEventListener("mouseover",function(event){
+            document.querySelector(".eightth").style.visibility="hidden";
+            array.push("8");
+            if(array.length==9){
+            alert("the order in which the numbers disappeared is:"+array);}});
+        //    array.push('8');
+            //for the ninth
+            document.querySelector(".ninth").addEventListener("mouseover",function(event){
+              document.querySelector(".ninth").style.visibility="hidden";
+              array.push("9");
+              if(array.length==9){
+              alert("the order in which the numbers disappeared is:"+array);}
+          //    array.push('9');
+});
+
+
+if(array.length==9){
+alert("the order in which the numbers disappeared is:"+array);}
+}
